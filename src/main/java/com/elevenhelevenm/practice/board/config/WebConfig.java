1a5b6daa7a4e2589f1da540781cc05e512cf94d8
jsj0718 <jeongsejin718@gmail.com>
2022-04-11 23:46:46 +0900

    SecurityConfig 설정

--- src/main/java/com/elevenhelevenm/practice/board/config/WebConfig.java
+++ src/main/java/com/elevenhelevenm/practice/board/config/WebConfig.java
@@ -2,16 +2,19 @@ package com.elevenhelevenm.practice.board.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .exposedHeaders("X-AUTH-TOKEN")
-                .allowCredentials(true)
-                .allowedOrigins("http://localhost:8080");
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setContentType("text/html;charset=UTF-8");
+
+        registry.viewResolver(resolver);
     }
 }
--- src/main/java/com/elevenhelevenm/practice/board/web/MemberController.java
+++ src/main/java/com/elevenhelevenm/practice/board/web/MemberController.java
@@ -4,9 +4,7 @@ import com.elevenhelevenm.practice.board.service.member.MemberService;
 import com.elevenhelevenm.practice.board.web.dto.MemberSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,8 +12,43 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    //SecurityConfig 파일 생성 이후 리다이렉트 작동 멈춤
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+/*
     @PostMapping("/join")
     public Long join(@RequestBody MemberSaveRequestDto requestDto) {
         return memberService.save(requestDto);
     }
+*/
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료";
+    }
+
+
 }
